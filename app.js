576e7cbaae86572fe10e48b42ce0d0602cce1df8
Allyson Reis <allysonreis1@gmail.com>
2021-07-17 20:53:32 -0300

    Permitindo acesso a api com cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -12,6 +14,22 @@ import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
 import imageRoutes from './src/routes/imageRoutes';
 
+const whiteList = [
+  'https://react1.otaviomiranda.com.br',
+  'https://react2.otaviomiranda.com.br',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -20,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
